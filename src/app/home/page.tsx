5529cf10d73c3853489727554c2c0852dfea004d
KrishnaYad <krishnayadav4684@gmail.com>
2025-09-13 16:40:29 +0530

    added rate limiting

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,22 +1,15 @@
 'use client'
 
-import { getConversation } from "@/actions/getConversation"
-import { GetUsers } from "@/actions/getUsers"
+import axios from "axios"
 import { signOut, useSession } from "next-auth/react"
 import { useEffect, useState } from "react"
 
 export default function Home() {
 
-    async function data() {
-    }
-
-
     useEffect(() => {
 
         const ws = new WebSocket("ws://localhost:3000/api/v1/ws");
-
         ws.onopen = () => { ws.send(`{"type":"join","content":"absck","recipientId":"cmfcn7gre0000g5bci49j3tt2"}`) }
-
         ws.onmessage = (event) =>
             console.log("Message from server:", event.data);
 
@@ -29,7 +22,14 @@ export default function Home() {
     return (<>
         {JSON.stringify(session)}
         <div>
-            <button onClick={() => { signOut() }}>LOG OUT</button>
+            <button onClick={async () => { await signOut() }}>LOG OUT</button>
+            <button onClick={async () => {
+                await axios.post('/api/v1/signup', {
+                    email: "krishna@yadav.com",
+                    password: "123456789",
+                    username:"krishnaCHU"
+                })
+            }}>data</button>
         </div>
     </>)
 }
---
+++ src/middleware.ts
@@ -0,0 +1,58 @@
+
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import rateLimit from "next-rate-limit";
+
+const minuteLimiter = rateLimit({
+    interval: 60 * 1000,
+    uniqueTokenPerInterval: 500,
+});
+
+const secondLimiter = rateLimit({
+    interval: 1000,
+    uniqueTokenPerInterval: 500
+})
+
+export default async function middleware(req: NextRequest) {
+
+    const path = req.nextUrl.pathname
+    let limit
+
+    if (path.startsWith('/api/v1/signup') || path.startsWith("/api/v1/otp-verify")) {
+        limit = 5
+        try {
+            await minuteLimiter.checkNext(req, limit);
+            return NextResponse.next();
+        } catch {
+            return NextResponse.json({ msg: "Rate limit exceeded" }, { status: 429 });
+        }
+    }
+
+    if (req.headers.has("next-action") && req.method === "POST") {
+        limit = 50
+        try {
+            await secondLimiter.checkNext(req, limit);
+            return NextResponse.next();
+        } catch {
+            return NextResponse.json({ msg: "Rate limit exceeded" }, { status: 429 });
+        }
+    }
+
+    if (path.startsWith("/api/v1/ws") || path.startsWith('/api/auth')) {
+        limit = 50
+        try {
+            await secondLimiter.checkNext(req, limit);
+            return NextResponse.next();
+        } catch {
+            return NextResponse.json({ msg: "Rate limit exceeded" }, { status: 429 });
+        }
+    }
+
+    return NextResponse.next()
+}
+
+
+export const config = {
+    matcher: "/:path*",
+};
+
